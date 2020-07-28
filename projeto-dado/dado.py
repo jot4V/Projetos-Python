348e7ede3b5c9444ebcee42f152dc37537418d20
João Victor Teixeira <jvtdrocha@gmail.com>
2020-07-28 18:08:00 -0300

    Create dado.py

---
+++ projeto-dado/dado.py
@@ -0,0 +1,13 @@
+#Jogo de Dados
+
+#***EM ANDAMENTO***
+
+#Importações
+from random import randint
+
+def jogo_dado():
+
+    #Colhendo nome dos usuários 
+    j1 = input('Nome do Primeiro Jogador: ')
+    j2 = input('Nome do Segundo Jogador: ')
+    
